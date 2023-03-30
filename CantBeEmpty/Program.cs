dcf8eb68830038b7e9f6c3b5773f5f1fe9b09b62
Éloi Strée <eloistree@gmail.com>
2023-03-30 14:43:29 +0200

    Save first draft version

---
+++ CantBeEmpty/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string errorHappened="";
+while (true) {
+    if (errorHappened.Length > 0)
+        Console.Write("ERROR:" + errorHappened);
+    Console.WriteLine("Give folder to target(empty is current):");
+    string folderPath = Console.ReadLine();
+    Console.WriteLine("Given:\""+folderPath+ "\"");
+    errorHappened = "";
+    folderPath = folderPath.Trim();
+    if (folderPath == null || folderPath.Length == 0) {
+        folderPath = Directory.GetCurrentDirectory();
+    }
+    int created = 0;
+    try
+    {
+        List<string> directories = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories).ToList();
+        directories.Add(folderPath);
+        foreach (var dir in directories)
+        {
+            if (Directory.Exists(dir))
+            {
+                string emptyFilePath = dir + "\\.notempty";
+                string[] files = Directory.GetFiles(dir);
+                files = files.Where(k => k != emptyFilePath).ToArray();
+                if (files.Length == 0)
+                {
+                    created++;
+                    if(!File.Exists(emptyFilePath))
+                        File.WriteAllText(emptyFilePath, "Not Empty");
+                }
+                if (files.Length > 0 && File.Exists(emptyFilePath))
+                {
+                    created++;
+                    File.Delete(emptyFilePath);
+                }
+            }
+        }
+        Console.WriteLine("Empty folder found .notempty file = " + created);
+    }
+    catch (Exception e) {
+        errorHappened = "Something when wrong  >>>>>>>>>>>\n" + e.StackTrace;
+    }
+}
